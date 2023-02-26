9b8ed6ca19b9a1a28dede786bf971c7db276c8c8
Zaky fetoh <zaky.fetoh@gmail.com>
2023-02-26 17:50:31 +0200

    some ENV is addded

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 const mongoose = require("mongoose");
 const express = require("express");
-const path = require("path")
+const path = require("path");
+const morgan = require("morgan");
 require("dotenv").config({
     path:path.join(__dirname,".env")
 });
 mongoose.pluralize(null);
 
+const PORT = process.env.PORT || 0 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 console.log(MONGODB_URI);
 (async()=>{
-    try{
-        await mongoose.connect(MONGODB_URI);
-        console.log(`Connectedto MONGODB at URI of : ${MONGODB_URI}`)
+    try{mongoose.connect(MONGODB_URI);
+        console.log(`Connectedto MONGODB at URI of : ${
+            MONGODB_URI}`)
     }catch(e){
         console.log("Cant connect to MONGODB")
         console.log(e.message);
         process.exit(14);
     }
+    const ser = express().use(morgan())
+    .use(express.json()).use(express.urlencoded())
+    .get("/about",(req, res,next)=>{
+        res.json({
+            name:"mahMoud zaky",
+            lastName:"Fetoh", 
+            email:"zaky.Fetoh@gmail.co",
+        })
+    }).listen(PORT,()=>{
+        console.log(`Blog Ser Is ${ser.address().port}`)
+    })
 })()
