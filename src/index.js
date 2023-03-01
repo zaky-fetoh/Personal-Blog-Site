1584c3a361bbb9e62aeb5bd0d8eeb20fdc6d7264
Zaky fetoh <zaky.fetoh@gmail.com>
2023-03-01 23:15:30 +0200

    Quary caching using radis is deved and ready for Use

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-require("./services/caching")
+const path = require("path");
+require("dotenv").config({
+    path: path.join(__dirname, ".env")
+});
+
+require("./services/caching-quary")
 
 const mongoose = require("mongoose");
 const express = require("express");
-const path = require("path");
 const morgan = require("morgan");
 mongoose.pluralize(null);
 
-require("dotenv").config({
-    path:path.join(__dirname,".env")});
 
 const userRoutes = require("./routes/user")
 const blogRoutes = require("./routes/blog")
@@ -17,27 +19,27 @@ const TimerMiddleware = require("./services/setTimer");
 const contAuth = require("./controller/Auth")
 
 
-const PORT = process.env.PORT || 0 
+const PORT = process.env.PORT || 0
 const MONGODB_URI = process.env.MONGODB_URI;
 
 console.log(MONGODB_URI);
-(async()=>{
-    try{mongoose.connect(MONGODB_URI);
-        console.log(`Connectedto MONGODB at URI of : ${
-            MONGODB_URI}`)
-    }catch(e){
+(async () => {
+    try {
+        mongoose.connect(MONGODB_URI);
+        console.log(`Connectedto MONGODB at URI of : ${MONGODB_URI}`)
+    } catch (e) {
         console.log("Cant connect to MONGODB")
         console.log(e.message);
         process.exit(14);
     }
     const ser = express().use(TimerMiddleware.usetimer).use(morgan())
-    .use(express.json()).use(express.urlencoded())
-    .use("/user", userRoutes)
-    .use("/blog",contAuth.vertify, blogRoutes)
-    .post("/login", contAuth.Login)
-    .get("/verify", contAuth.vertify)
+        .use(express.json()).use(express.urlencoded())
+        .use("/user", userRoutes)
+        .use("/blog", contAuth.vertify, blogRoutes)
+        .post("/login", contAuth.Login)
+        .get("/verify", contAuth.vertify)
 
-    .listen(PORT,()=>{
-        console.log(`Blog Ser Is ${ser.address().port}`)
-    })
+        .listen(PORT, () => {
+            console.log(`Blog Ser Is ${ser.address().port}`)
+        })
 })()
--- src/services/caching-quary.js
+++ src/services/caching-quary.js
@@ -1,27 +1,31 @@
 const mongoose = require("mongoose");
 const redis = require("redis");
-const util = require("util");
 
 const REDIS_URI = process.env.REDIS_URI;
 
-const origiExec = mongoose.Query.prototype.exec
-
-await (async()=>{
-
-
-
-
-const redisClient = await (redis.createClient({
-    url: REDIS_URI,
-}).connect())
-
-const Rget = util.promisify(redisClient.get).bind(redisClient);
-const Rset = util.promisify(redisClient.set).bind(redisClient);
+const origiExec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.cachable = function () {
     this.useCache = true;
     return this
-}
+};
+
+const redisClient = redis.createClient({
+    url: REDIS_URI,
+});
+
+(async () => {
+    await redisClient.connect();
+})()
+
+redisClient.on("error", (e) => {
+    console.log(e.message);
+    process.exit(20);
+});
+
+redisClient.on('ready', () => {
+    console.log(`Redis Caching Server is Connected! at ${REDIS_URI}`);
+});
 
 mongoose.Query.prototype.exec = async function () {
 
@@ -39,19 +43,24 @@ mongoose.Query.prototype.exec = async function () {
     // if (!this.useCache)
     //     return origiExec.apply(this, arguments)
 
-    // this.getOptions()
+
     const CollName = this.model.collection.collectionName;
     const Qkey = JSON.stringify({ ...this.getFilter(), CollName });
 
-    try {
-        const value = await Rget(Qkey)
-        console.log(value)
-    } catch (e) {
-        console.log(e.message)
+    const value = await redisClient.get(Qkey)
+    if (value) {
+        const parsedCache = JSON.parse(value);
+        if(Array.isArray(parsedCache)) return parsedCache.map(e=>{
+            return this.model(e)
+        }); 
+        else return this.model(parsedCache)
     }
 
     const outPut = await origiExec.apply(this, arguments);
-
+    (async()=>{
+        await redisClient.set(Qkey, JSON.stringify(outPut),{
+            EX:5,
+        })
+    })()
     return outPut
 }
-})()
