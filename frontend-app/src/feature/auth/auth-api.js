781e53ff3edab04a47ccc3217fc22daad8904cf5
Zaky fetoh <zaky.fetoh@gmail.com>
2023-03-06 16:57:13 +0200

    orinization

--- frontend-app/src/feature/auth/auth-api.js
+++ frontend-app/src/feature/auth/auth-api.js
@@ -1,51 +1,51 @@
 const UrlApi = "http://localhost:3000/"
 
 
-export const login = async({userName, password})=>{
-    const req =await fetch(UrlApi+"Login",{
-        method:"POST", 
-        body:JSON.stringify({
+export const login = async ({ userName, password }) => {
+    const req = await fetch(UrlApi + "Login", {
+        method: "POST",
+        body: JSON.stringify({
             userName, password
         }),
-        headers:{
-            "Content-Type":"application/json",
+        headers: {
+            "Content-Type": "application/json",
             'Access-Control-Allow-Origin': '*',
         },
     })
-    const reqjson= await req.json()
+    const reqjson = await req.json()
     // console.log(reqjson)
-    if(reqjson.ok) return reqjson;
+    if (reqjson.ok) return reqjson;
     else throw new Error(reqjson.message);
 }
 
-export const signUp = async({userName, password})=>{
-    const req =await fetch(UrlApi+"user",{
-        method:"POST", 
-        body:JSON.stringify({
+export const signUp = async ({ userName, password }) => {
+    const req = await fetch(UrlApi + "user", {
+        method: "POST",
+        body: JSON.stringify({
             userName, password
         }),
-        headers:{
-            "Content-Type":"application/json",
+        headers: {
+            "Content-Type": "application/json",
             'Access-Control-Allow-Origin': '*',
         },
     })
-    const reqjson= await req.json()
-    if(reqjson.ok) return reqjson;
+    const reqjson = await req.json()
+    if (reqjson.ok) return reqjson;
     else throw new Error(reqjson.message);
 }
 
 
 
-export const verifyToken = async()=>{
+export const verifyToken = async () => {
     const token = localStorage.getItem("token");
-    const res = await fetch(UrlApi+"verify",{
-        method:"GET",
-        headers:{
+    const res = await fetch(UrlApi + "verify", {
+        method: "GET",
+        headers: {
             "Authorization": `Bearer ${token}`,
             'Access-Control-Allow-Origin': '*',
         }
     })
-    const data =  await res.json(); 
-    if(data.ok) return true;
+    const data = await res.json();
+    if (data.ok) return true;
     else throw new Error(data.message)
 }
--- pushScr.sh
+++ pushScr.sh
@@ -1,3 +1,3 @@
-git add .
-git commit -a -m "$1"
-git push origin master
+git add .;
+git commit -a -m "$1";
+git push -u origin master;
