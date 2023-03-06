00ee67fd8c0166cb8cd47eab8abcf10b9736b322
Zaky fetoh <zaky.fetoh@gmail.com>
2023-03-07 00:41:09 +0200

    generalStructure is added

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -2,13 +2,40 @@ import AuthPage from "./pages/AuthPage";
 import {actions as authAct} from "./feature/auth/auth-slice"
 import{ useDispatch, useSelector }from "react-redux"
 import { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import styles from "./App.module.css"
 
+function Name(props){
+  return <div>
+      <p>ZAky</p>
+  </div>
+}
+
+function NavHeader(){
+  return <div className={styles.headerDiv}><nav>
+      <Link to="/myBlogs">  Profile  </Link>|
+      <Link to="/logout">  LogOut  </Link>
+    </nav></div>
+}
+
+function LogOut(props){
+  const dispatch = useDispatch();
+  dispatch(authAct.logout());
+  return <div>
+    Loging Out...
+  </div>
+}
+
 function Home(props){
-  return <h1>
-    hello
-  </h1>
+  return <><header>
+  <Name/> <NavHeader/>
+  </header> <Routes>
+    <Route index element={<BlogHeaderPage/>} />
+    <Route path="/myBlogs" element={<MyBlogsPage/>}/>
+    <Route path="/:blogId" element={<BlogPage/>}/>
+    <Route path="/logout" element={<LogOut/>}/>
+    <Route path="/*" element={<NotFoundPage/>}/>
+  </Routes></>
 }
 
 
